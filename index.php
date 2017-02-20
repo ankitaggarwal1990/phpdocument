375446b7206640565e9f49eb34cba478be3903f6
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-20 14:46:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-echo "asadAD12123221";
-$con = mysqli_connect('trident1.database.windows.net,1433','trident','password@123','database_azure');
-if (!$con) {
-    die('Could not connect: ' . mysqli_error($con));
-}
-else{
-	echo "connect";
-}
+echo "abc";
 // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:trident1.database.windows.net,1433; Database = database_azure", "trident", "password@123");
