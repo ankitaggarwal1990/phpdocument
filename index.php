a07c93084e3d4e073e9e8ae791463096dd9b9306
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-20 14:37:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
-echo "ASD";
-
-
-
+echo "asadAD";
+$con = mysqli_connect('127.0.0.1:49646','azure','6#vWHD_$','localdb');
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+else{
+	echo "connect";
+}
 
 
 ?>
