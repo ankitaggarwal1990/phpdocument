1a4f329497c87f805645bcd8b5a2a8495cd51585
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-20 14:41:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <?php
 echo "asadAD";
-$con = mysqli_connect('127.0.0.1:49646','azure','6#vWHD_$','localdb');
+$con = mysqli_connect('','trident','password@123','database_azure');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 else{
 	echo "connect";
 }
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:trident1.database.windows.net,1433; Database = database_azure", "trident", "password@123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "trident@trident1", "pwd" => "password@123", "Database" => "database_azure", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:trident1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
+if (!$conn) {
+    die('Could not connect11111 ');
+}
+else{
+	echo "connect11111";
+}
 
 ?>
