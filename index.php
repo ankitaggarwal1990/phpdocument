6f1c7d4c7d214a9dc7ceada299c518ad5eadd38f
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-20 15:12:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "abc";
+//echo "abc";
 // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:trident1.database.windows.net,1433; Database = database_azure", "trident", "password@123");
@@ -9,30 +9,126 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
-
 // SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => "trident@trident1", "pwd" => "password@123", "Database" => "database_azure", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:trident1.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-
 if (!$conn) {
     die('Could not connect11111 ');
 }
 else{
-	echo "connect11111";
+	//echo "connect11111";
 }
 
+?>
 
+
+
+
+
+
+
+<html>
+<head>
+
+
+<style> 
+input[type=text] {
+    width: 130px;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+    background-color: white;
+    background-image: url('searchicon.png');
+    background-position: 10px 10px; 
+    background-repeat: no-repeat;
+    padding: 12px 20px 12px 40px;
+    -webkit-transition: width 0.4s ease-in-out;
+    transition: width 0.4s ease-in-out;
+}
+input[type=text]:focus {
+    width: 100%;
+}
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+th, td {
+    text-align: left;
+    padding: 8px;
+}
+tr:nth-child(even){background-color: #f2f2f2}
+th {
+	text-align: center;
+    background-color: #4CAF50;
+    color: white;
+}
+td {
+    text-align: center; /* center checkbox horizontally */
+    vertical-align: middle; /* center checkbox vertically */
+}
+.button {
+    background-color: #4CAF50; /* Green */
+    
+    color: white;
+    
+    text-align: center;
+    
+} 
+.select{
+	
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+    background-color: white;
+    background-image: url('searchicon.png');
+    background-position: 10px 10px; 
+    background-repeat: no-repeat;
+    padding: 12px 20px 12px 40px;
+    -webkit-transition: width 0.4s ease-in-out;
+    transition: width 0.4s ease-in-out;
+}
+</style>
+</head>
+
+
+<body>
+
+<?php 
+//echo $value;
 $sql = "SELECT * from runtime_data";
-
 $stmt = sqlsrv_query( $conn, $sql );
 if( $stmt === false) {
     die( print_r( sqlsrv_errors(), true) );
 }
-
-while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
-      echo $row['deviceid'].", ".$row['GrossWeight']."<br />";
-}
-
 ?>
+<div>
+<table> 
+<tr>
+<th>DEVICE ID</th>
+<th>TAG ID</th>
+<th>GROSS WEIGHT</th>
+<th>TIME STAMP</th>
+</tr>
+<tr>
+<?php
+while($row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC)) {
+?>
+<td><?php $row['deviceid'] ?></td>
+<td><?php $row['TagID'] ?></td>
+<td><?php $row['GrossWeight'] ?></td>
+<td><?php $row['time_stamp'] ?></td>
+
+<?php
+}
+?>
+</tr>
+
+</table>
+
+</div>
+
+</body>
+</html>
