552d0b7f47eb07b9f46dacc25a9692721b6c552e
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-24 12:18:16 +0530

    Update a.php

--- a.php
+++ a.php
@@ -1,9 +1,12 @@
+<?php
+
 include 'documentdb-for-php.php';
 $host = 'https://trackandtracerescuieda5a28.documents.azure.com:443/'; // see (1) in the screenshots bellow
 $master_key = 'CfSDcA4As7qzpjyA1eeK1SFTIuXvQSyHfZC63g8aMDDcXHaDCeyC6fcW4E1qxm0ztz7LI3vT7gxgWbRZDCULiQ=='; // see (2) in the screenshots bellow
 $db = 'DevMgmtDB'; // see (3) in the screenshots bellow (optional)
 $db_rid = 'thedbresourceid=='; // see (4) in the screenshots bellow
-$coll = 'yourcollectionname'; // see (5) in the screenshots bellow (optional)
+$coll = 'DevMgmtCollection'; // see (5) in the screenshots bellow (optional)
 $coll_rid = 'yourcollectionresourceid='; // see (6) in the screenshots bellow
-$query = "Select * from ..."; // This is your SQL or LINQ code
+$query = "Select * from DevMgmtCollection"; // This is your SQL or LINQ code
 querycoll($host, $db_rid, $coll_rid, $query,$apptype,$useragent,$cachecontrol,$da_date,$api_version,$master,$token,$master_key,$da_date); //The function that does the magic. All variables are defined above and in the functions file
+?>
